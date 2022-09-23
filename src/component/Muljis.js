f82c411c7063a9624c23daa7d749535735395d50
deepakchauhan2096 <deepakchauhan2096@gmail.com>
2022-09-23 13:14:18 +0530

    clientdata

---
+++ src/component/Muljis.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './style.css'
+
+const Muljis = () => {
+  return (
+    <>
+    <div className="container">
+    <div className="row">
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+            <div className='col border border-dark p-5  box m-1'>1</div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Muljis
